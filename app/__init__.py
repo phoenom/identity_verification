d9e7d46c4bc495ad1abe7dbf339d5b38cfab2832
fajara <whitephoenom@gmail.com>
2020-10-03 20:03:18 +0700

    update frontend

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,20 @@ from flask import Flask, render_template
 
 # def create_app(test_config=None):
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-@app.route('/hello')
-def hello():
-    return "Hello World"
+# @app.route('/hello')
+# def hello():
+#     return "Hello World"
 
 @app.route('/')
 def index():
-    print(os.getcwd())
     return render_template("index.html", image="static/pentol.png")
+
+
+@app.route('/new-label')
+def label():
+    print("oke")
+    # print(os.getcwd())
+    # return render_template("index.html", image="static/pentol.png")
 
